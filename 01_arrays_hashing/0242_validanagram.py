eeebfe6999a36eb02b18facff76ce8aa99e94863
Manu Jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-01-04 16:49:31 -0800

    Valid Anagram

--- 01_arrays_hashing/0242_validanagram.py
+++ 01_arrays_hashing/0242_validanagram.py
@@ -2,21 +2,39 @@ from typing import List
 from collections import defaultdict
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!= len(t):
-            return False
 
-        hashmapp = defaultdict(int)
-        hashmapp1 = defaultdict(int)
-        for i in range(len(s)):
-            hashmapp[s[i]] += 1
-            hashmapp1[t[i]] += 1
-        
-        for char in s:
-            if hashmapp[char] != hashmapp1[char]:
+        if len(s) != len(t): return False
+
+        s_count = [0]*26
+        t_count = [0]*26
+
+        for i in range(len(t)):
+            s_count[ord(s[i])-ord('a')]+=1
+            t_count[ord(t[i])-ord('a')]+=1
+        print(s_count,t_count)
+        return "".join(map(str,s_count)) == "".join(map(str,t_count))
+    
+
+    #Further optimisation
+    def is_Anagram_Optimized(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+        char_count = [0]*26
+        for i in range(len(t)):
+            char_count[ord(s[i])-ord('a')]+=1
+            char_count[ord(t[i])-ord('a')]-=1
+
+        for char in char_count:
+            if char!=0:
                 return False
         return True
+
+    
         
 
 solu = Solution()
-res = solu.isAnagram("anagram","nagaram")
+res = solu.isAnagram("anagram","nagaraaam")
 print(res)
+
+
+res1 = solu.is_Anagram_Optimized("anagram","nagaram")
+print(res1)
