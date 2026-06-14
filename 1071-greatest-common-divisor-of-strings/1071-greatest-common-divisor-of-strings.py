5ab7c9de2f3e127c444ab46739563afbc7b0550e
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-06-14 11:14:01 -0700

    Time: 3 ms (25.18%), Space: 19.3 MB (33.47%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,17 +1,7 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        N,M = len(str1), len(str2)
+        if str1+str2 != str2+str1:
+            return ""
         
-        def valid(t):
-            if N%t !=0 or M%t != 0:
-                return False
-            
-            N1, M1 = N//t, M//t
-            base = str1[:t]
-
-            return str1 == N1*base and str2==M1*base
-            
-        for i in range(min(M,N), 0, -1):
-            if valid(i):
-                return str1[:i]
-        return ""
+        max_length = gcd(len(str1), len(str2))
+        return str1[:max_length]
