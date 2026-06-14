486b6ed706e88d90401403a8fef9eafdfd1bdfad
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-06-13 20:47:18 -0700

    Time: 40 ms (37.21%), Space: 53.7 MB (16.07%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,12 +4,43 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def end_of_first_half(self, head: ListNode):
+        fast = head
+        slow = head
+        while fast.next is not None and fast.next.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+    
+    def reverse_list(self, head:ListNode):
+        previous = None
+        current = head
+        while current is not None:
+            next_node = current.next
+            current.next = previous
+            previous = current
+            current = next_node
+        return previous
+    
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        array = []
-        current_node = head
-        while current_node is not None:
-            array.append(current_node.val)
-            current_node = current_node.next
-        
-        return array == array[::-1]
+        if head is None: return False
+
+        first_half_end = self.end_of_first_half(head)
+        second_half_start = self.reverse_list(first_half_end.next)
+
+        res = True
+        first_position = head
+        second_position = second_half_start
+
+        while res and second_position is not None:
+            if first_position.val != second_position.val:
+                res = False
+            
+            first_position = first_position.next
+            second_position = second_position.next
+
+        first_half_end.next = self.reverse_list(second_half_start)
+        return res
+
+
         
