8a181ed9d4a681bd29e7d22aed8c6098449201c6
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-07-03 16:28:29 -0700

    Time: 319 ms (91.67%), Space: 0B (100%) - LeetHub

--- 1355-activity-participants/1355-activity-participants.sql
+++ 1355-activity-participants/1355-activity-participants.sql
@@ -1,12 +1,14 @@
 /* Write your T-SQL query statement below */
 WITH Activity_Count AS (
     SELECT activity,
-           COUNT(id) AS activity_count
+           COUNT(id) AS activity_count,
+           MAX(COUNT(id)) OVER() AS max_count,
+           MIN(COUNT(id)) OVER() AS min_count
     FROM Friends
     GROUP BY activity
 )
 
-
+-- SELECT * FROM Activity_Count
 SELECT activity FROM Activity_Count WHERE
-activity_count < (SELECT MAX(activity_count) AS maxx FROM Activity_Count) AND 
-activity_count > (SELECT MIN(activity_count) AS minn FROM Activity_Count)
+activity_count < max_count AND 
+activity_count > min_count
