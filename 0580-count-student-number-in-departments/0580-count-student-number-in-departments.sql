21de63d13ecc4359da428ab2363ea463e7c7c360
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-07-03 12:20:08 -0700

    Time: 497 ms (30%), Space: 0B (100%) - LeetHub

---
+++ 0580-count-student-number-in-departments/0580-count-student-number-in-departments.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+
+SELECT D.dept_name,
+       COUNT(DISTINCT S.student_id) AS student_number
+FROM Department D
+LEFT OUTER JOIN Student S
+ON D.dept_id = S.dept_id
+GROUP BY D.dept_name
+ORDER BY COUNT(DISTINCT S.student_id) DESC, D.dept_name
