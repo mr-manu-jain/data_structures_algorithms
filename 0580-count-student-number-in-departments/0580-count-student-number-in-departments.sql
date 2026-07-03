f32db3c8e2efd509042c346890edcdfddb329852
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-07-03 12:40:32 -0700

    Time: 559 ms (15.38%), Space: 0B (100%) - LeetHub

--- 0580-count-student-number-in-departments/0580-count-student-number-in-departments.sql
+++ 0580-count-student-number-in-departments/0580-count-student-number-in-departments.sql
@@ -1,7 +1,7 @@
 /* Write your T-SQL query statement below */
 
 SELECT D.dept_name,
-       COUNT(DISTINCT S.student_id) AS student_number
+       COUNT(S.student_id) AS student_number
 FROM Department D
 LEFT OUTER JOIN Student S
 ON D.dept_id = S.dept_id
