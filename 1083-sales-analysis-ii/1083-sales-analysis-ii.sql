19b84cb88c36871cfa83c6878203440c2d4070d5
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-07-03 17:03:18 -0700

    Time: 2149 ms (86.67%), Space: 0B (100%) - LeetHub

--- 1083-sales-analysis-ii/1083-sales-analysis-ii.sql
+++ 1083-sales-analysis-ii/1083-sales-analysis-ii.sql
@@ -1,9 +1,9 @@
 WITH product_sales AS (
-    SELECT DISTINCT A.buyer_id,
+    SELECT A.buyer_id,
                     SUM(CASE WHEN B.product_name = 'S8' THEN 1 ELSE 0 END) AS S8_Sales,
                     SUM(CASE WHEN B.product_name = 'iPhone' THEN 1 ELSE 0 END) AS iPhone_Sales
     FROM Sales AS A
-    LEFT OUTER JOIN Product AS B
+    INNER JOIN Product AS B
     ON A.product_id = B.product_id
     GROUP BY A.buyer_id
 )
