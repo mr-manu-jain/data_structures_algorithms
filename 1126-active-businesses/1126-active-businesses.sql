cadae54cfac44101cd902a6ee8a27e8cd8947096
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-07-05 12:09:08 -0700

    Time: 2977 ms (23.81%), Space: 0B (100%) - LeetHub

---
+++ 1126-active-businesses/1126-active-businesses.sql
@@ -0,0 +1,19 @@
+/* Write your T-SQL query statement below */
+WITH averages AS (
+    SELECT event_type,
+           ROUND(SUM(occurrences)*1.0/COUNT(business_id),1) AS avg_activity
+    FROM Events
+    GROUP BY event_type
+),
+business_averages AS (
+    SELECT DISTINCT business_id,
+    COUNT(*) AS cnt
+    FROM Events
+    LEFT OUTER JOIN averages 
+    ON Events.event_type = averages.event_type
+    WHERE Events.occurrences > avg_activity
+    GROUP BY business_id
+    HAVING COUNT(*) > 1
+)
+
+SELECT business_id FROM business_averages
