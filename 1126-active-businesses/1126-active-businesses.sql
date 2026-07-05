aa90f16a46b6d42000bc2898eedeb95a3e802e83
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-07-05 12:15:19 -0700

    Time: 2708 ms (38.1%), Space: 0B (100%) - LeetHub

--- 1126-active-businesses/1126-active-businesses.sql
+++ 1126-active-businesses/1126-active-businesses.sql
@@ -1,19 +1,32 @@
-/* Write your T-SQL query statement below */
-WITH averages AS (
-    SELECT event_type,
-           ROUND(SUM(occurrences)*1.0/COUNT(business_id),1) AS avg_activity
+WITH CTE AS (SELECT business_id,
+    event_type,
+    occurrences,
+    AVG(occurrences) OVER(PARTITION BY event_type) AS avg
     FROM Events
-    GROUP BY event_type
-),
-business_averages AS (
-    SELECT business_id,
-    COUNT(*) AS cnt
-    FROM Events
-    LEFT OUTER JOIN averages 
-    ON Events.event_type = averages.event_type
-    WHERE Events.occurrences > avg_activity
-    GROUP BY business_id
-    HAVING COUNT(*) > 1
 )
 
-SELECT business_id FROM business_averages
+SELECT business_id
+FROM CTE
+WHERE occurrences > avg
+GROUP BY business_id
+HAVING COUNT(*) > 1
+
+-- /* Write your T-SQL query statement below */
+-- WITH averages AS (
+--     SELECT event_type,
+--            ROUND(SUM(occurrences)*1.0/COUNT(business_id),1) AS avg_activity
+--     FROM Events
+--     GROUP BY event_type
+-- ),
+-- business_averages AS (
+--     SELECT business_id,
+--     COUNT(*) AS cnt
+--     FROM Events
+--     LEFT OUTER JOIN averages 
+--     ON Events.event_type = averages.event_type
+--     WHERE Events.occurrences > avg_activity
+--     GROUP BY business_id
+--     HAVING COUNT(*) > 1
+-- )
+
+-- SELECT business_id FROM business_averages
