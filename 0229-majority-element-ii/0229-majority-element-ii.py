cf31af843db5ac9a1905c475180019df940a6620
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-07-08 14:06:58 -0700

    Time: 16 ms (10.26%), Space: 22.4 MB (56.68%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,10 +1,24 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        counter_ = Counter(nums)
+        count = defaultdict(int)
+        for num in nums:
+            count[num]+=1
+
+            if len(count) <=2:
+                continue
+            
+            #we cant' delete from the count directly as we are iterating through it, it will create issues. so that's why we are cerating a new dict. assigning it the curr val-1 and then making count equals to the new_count
+            new_count = defaultdict(int)
+            for num,c in count.items():
+                if c>1:
+                    new_count[num] = c-1
+            count = new_count
+
         res = []
-        for num,freq in counter_.items():
-            if freq > len(nums)/3:
-                res.append(num)
-
-
+        for num in count:
+            if nums.count(num) > len(nums)//3:                    res.append(num)
         return res
+
+        
+
+            
