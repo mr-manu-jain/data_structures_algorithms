abf8db3e5018541ba15c47b6dd21b1d9d8065e35
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-07-10 23:53:16 -0700

    Time: 244 ms (100%), Space: 0B (100%) - LeetHub

--- 1303-find-the-team-size/1303-find-the-team-size.sql
+++ 1303-find-the-team-size/1303-find-the-team-size.sql
@@ -1,6 +1,10 @@
 /* Write your T-SQL query statement below */
-WITH CTE AS (SELECT team_id, COUNT(employee_id) AS team_size FROM Employee GROUP BY team_id)
 
 SELECT employee_id,
-       C.team_size
-FROM Employee LEFT OUTER JOIN CTE C ON Employee.team_id = C.team_id
+       COUNT(team_id) OVER(PARTITION BY team_id) team_size
+FROM Employee
+-- WITH CTE AS (SELECT team_id, COUNT(employee_id) AS team_size FROM Employee GROUP BY team_id)
+
+-- SELECT employee_id,
+--        C.team_size
+-- FROM Employee LEFT OUTER JOIN CTE C ON Employee.team_id = C.team_id
