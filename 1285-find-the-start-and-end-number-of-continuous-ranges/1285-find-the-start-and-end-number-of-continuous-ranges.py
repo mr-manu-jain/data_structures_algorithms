a8d0a2eafa5cd3ea9919c4408f148c75b16e8f40
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-07-15 12:23:23 -0700

    Time: 311 ms (35.29%), Space: 67.6 MB (60.78%) - LeetHub

--- 1285-find-the-start-and-end-number-of-continuous-ranges/1285-find-the-start-and-end-number-of-continuous-ranges.py
+++ 1285-find-the-start-and-end-number-of-continuous-ranges/1285-find-the-start-and-end-number-of-continuous-ranges.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def find_continuous_ranges(logs: pd.DataFrame) -> pd.DataFrame:
-    logs = logs.sort_values("log_id", ignore_index = True)
+    # logs = logs.sort_values("log_id", ignore_index = True)
     return logs.groupby(logs.log_id - logs.index).agg(start_id=("log_id","min"), end_id=("log_id","max"))
