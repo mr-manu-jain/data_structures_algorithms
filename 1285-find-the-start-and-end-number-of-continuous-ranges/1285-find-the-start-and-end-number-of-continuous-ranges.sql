c07dcb49999b0ab4b83f5b3cc9351c79ef0fbe8a
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-07-15 12:17:23 -0700

    Time: 263 ms (86.49%), Space: 0B (100%) - LeetHub

--- 1285-find-the-start-and-end-number-of-continuous-ranges/1285-find-the-start-and-end-number-of-continuous-ranges.sql
+++ 1285-find-the-start-and-end-number-of-continuous-ranges/1285-find-the-start-and-end-number-of-continuous-ranges.sql
@@ -2,14 +2,11 @@
 WITH CTE AS (
     SELECT ROW_NUMBER() OVER (ORDER BY log_id) AS s_no,
         log_id
-        -- LEAD(log_id) OVER (ORDER BY log_id) AS next_log,
-        -- LAG(log_id) OVER (ORDER BY log_id) AS prev_log
     FROM Logs
     GROUP BY log_id
-),
-grouping AS (
-SELECT *, 
-       log_id-s_no AS diff1
-FROM CTE)
+)
 
-SELECT MIN(log_id) AS start_id, MAX(log_id) AS end_id FROM grouping GROUP BY diff1
+SELECT MIN(log_id) AS start_id, 
+        MAX(log_id) AS end_id 
+FROM CTE 
+GROUP BY log_id-s_no 
