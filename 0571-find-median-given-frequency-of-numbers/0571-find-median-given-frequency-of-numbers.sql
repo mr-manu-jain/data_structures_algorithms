10e740a979bf045dc92f5e3e7bd08694d3b06754
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-07-16 23:42:06 -0700

    Time: 334 ms (90.48%), Space: 0B (100%) - LeetHub

--- 0571-find-median-given-frequency-of-numbers/0571-find-median-given-frequency-of-numbers.sql
+++ 0571-find-median-given-frequency-of-numbers/0571-find-median-given-frequency-of-numbers.sql
@@ -1,13 +1,25 @@
-/* Write your T-SQL query statement below */
-WITH running_sum AS (
-    SELECT 
-        num,
-        frequency,
-        SUM(frequency) OVER (ORDER BY num ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS running_total,
-        SUM(frequency) OVER () AS total_sum
-FROM Numbers
+-- /* Write your T-SQL query statement below */
+
+WITH Temp AS (
+    SELECT num,
+           frequency,
+           SUM(frequency) OVER (ORDER BY num ASC) AS rnk1,
+           SUM(frequency) OVER (ORDER BY num DESC) AS rnk2
+    FROM Numbers
 )
 
-SELECT CAST(AVG(1.0*num) AS DECIMAL(10,1)) AS median 
-FROM running_sum 
-WHERE total_sum/2.0 BETWEEN (running_total-frequency) AND running_total
+SELECT AVG(num * 1.0) AS median
+FROM Temp
+WHERE ABS(rnk1-rnk2) <= frequency
+-- WITH running_sum AS (
+--     SELECT 
+--         num,
+--         frequency,
+--         SUM(frequency) OVER (ORDER BY num ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS running_total,
+--         SUM(frequency) OVER () AS total_sum
+-- FROM Numbers
+-- )
+
+-- SELECT CAST(AVG(1.0*num) AS DECIMAL(10,1)) AS median 
+-- FROM running_sum 
+-- WHERE total_sum/2.0 BETWEEN (running_total-frequency) AND running_total
