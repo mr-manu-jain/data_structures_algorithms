6899ae7ab651b0d44ef08f6ec3c452513d6a600d
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-07-26 20:20:32 -0700

    Time: 1248 ms (79.17%), Space: 0B (100%) - LeetHub

---
+++ 1097-game-play-analysis-v/1097-game-play-analysis-v.sql
@@ -0,0 +1,46 @@
+/* Write your T-SQL query statement below */
+WITH first_install_dt AS (
+    SELECT player_id,
+    MIN(event_date) AS first_install_date
+    FROM Activity
+    GROUP BY player_id
+),
+install_counts AS (
+    SELECT first_install_date AS install_dt,
+    COUNT(DISTINCT player_id) AS installs
+    FROM first_install_dt
+    GROUP BY first_install_date
+),
+next_install_dates AS (
+    SELECT player_id,
+           event_date,
+           DENSE_RANK() OVER (PARTITION BY player_id ORDER BY event_date ASC) AS rnk
+    FROM Activity
+),
+first_second AS (
+    SELECT player_id,
+           MAX(CASE WHEN rnk = 1 THEN event_date END) AS first_login,
+           MAX(CASE WHEN rnk = 2 THEN event_date END) AS second_login
+    FROM next_install_dates
+    GROUP BY player_id
+),
+datediff AS (
+    SELECT player_id,
+
+           DATEDIFF(day,first_login,second_login) AS difference
+    FROM first_second
+
+)
+SELECT ic.install_dt,
+       ic.installs,
+       ROUND(
+           SUM(CASE WHEN DATEDIFF(day, fs.first_login, fs.second_login) = 1 THEN 1 ELSE 0 END) * 1.0 
+           / ic.installs, 2
+       ) AS Day1_retention
+    --    fs.player_id,
+    --    fs.first_login,
+    --    fs.second_login
+FROM install_counts ic
+JOIN first_second fs ON ic.install_dt = fs.first_login
+GROUP BY ic.install_dt, ic.installs
+ORDER BY ic.install_dt;
