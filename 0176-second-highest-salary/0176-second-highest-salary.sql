c8e8820c364fd76557ce52f7c524b4ee8bcacac6
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-07-28 20:38:52 -0700

    Time: 456 ms (21.6%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,14 +1,10 @@
+SELECT MAX(salary) AS SecondHighestSalary FROM Employee WHERE salary < (
+    SELECT MAX(salary) FROM Employee
+)
+
 -- WITH CTE AS (
 --     SELECT id, salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rank
 --     FROM Employee
 -- )
 
 -- SELECT MAX(salary) AS SecondHighestSalary FROM CTE WHERE rank = 2
-
-
-
-WITH CTE AS (
-    SELECT id, salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rank FROM Employee
-)
-
-SELECT MAX(salary) AS SecondHighestSalary FROM CTE WHERE rank = 2
