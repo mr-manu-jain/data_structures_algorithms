f9c7f51276893bd580c6c954b90011b10c354adb
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-09-01 23:00:18 -0700

    Time: 576 ms (91.72%), Space: 0B (100%) - LeetHub

---
+++ 1468-calculate-salaries/1468-calculate-salaries.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+WITH max_salaries AS (
+    SELECT company_id,
+           MAX(salary) AS max_salary
+    FROM Salaries
+    GROUP BY company_id
+)
+SELECT A.company_id,
+        A.employee_id,
+        A.employee_name,
+        CASE WHEN B.max_salary  < 1000 THEN A.salary
+        WHEN B.max_salary>=1000 AND B.max_salary <=10000 THEN ROUND(A.salary*0.76,0)
+        ELSE ROUND(A.salary*0.51,0) END AS salary
+FROM Salaries AS A LEFT OUTER JOIN max_salaries AS B ON A.company_id = B.company_id
