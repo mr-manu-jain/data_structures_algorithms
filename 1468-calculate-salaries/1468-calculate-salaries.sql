7810c0025f0f5d9ea7b2860e29d80355cb7063cb
mr-manu-jain <159678251+mr-manu-jain@users.noreply.github.com>
2026-09-01 23:01:08 -0700

    Time: 604 ms (76.55%), Space: 0B (100%) - LeetHub

--- 1468-calculate-salaries/1468-calculate-salaries.sql
+++ 1468-calculate-salaries/1468-calculate-salaries.sql
@@ -11,4 +11,4 @@ SELECT A.company_id,
         CASE WHEN B.max_salary  < 1000 THEN A.salary
         WHEN B.max_salary>=1000 AND B.max_salary <=10000 THEN ROUND(A.salary*0.76,0)
         ELSE ROUND(A.salary*0.51,0) END AS salary
-FROM Salaries AS A LEFT OUTER JOIN max_salaries AS B ON A.company_id = B.company_id
+FROM Salaries AS A INNER JOIN max_salaries AS B ON A.company_id = B.company_id
